b2928105df8f7cfe6c88314d866a89f895ad1c70
jose.ocana <jose.ocana@i3television.es>
2022-08-03 16:42:01 +0200

    Mostrando en el carrito el curso seleccionado

--- 15-PROYECTO-Carrito/js/app.js
+++ 15-PROYECTO-Carrito/js/app.js
@@ -2,14 +2,55 @@ const carrito = document.querySelector('#carrito');
 const contenedorCarrito = document.querySelector('#carrito tbody');
 const vaciarCarritoBtn = document.querySelector('#vaciar-carrito');
 const listaCursos = document.querySelector('#lista-cursos');
+let articulosCarrito = [];
 
 cargarEventListeners();
 function cargarEventListeners() {
   listaCursos.addEventListener('click', agregarCurso);
 }
 
+// Agrega un nuevo curso al carrito
 function agregarCurso(e) {
+  e.preventDefault();
+
   if (e.target.classList.contains('agregar-carrito')) {
-    console.log('Añadiendo curso', e.target.classList);
+    const cursoSeleccionado = e.target.parentNode.parentNode;
+    obtenerDatosCurso(cursoSeleccionado);
+  }
+}
+
+//Obtener datos del curso
+function obtenerDatosCurso(curso) {
+  const datosCurso = {
+    id: curso.querySelector('a').getAttribute('data-id'),
+    titulo: curso.querySelector('h4').textContent,
+    imagen: curso.querySelector('img').getAttribute('src'),
+    precio: curso.querySelector('.precio span').textContent,
+    cantidad: 1
+  };
+
+  articulosCarrito = [...articulosCarrito, datosCurso];
+
+  carritoHTML();
+}
+
+// añade el HTML de los cursos al carrito
+function carritoHTML() {
+  limpiarHTML();
+
+  articulosCarrito.forEach(curso => {
+    const row = document.createElement('tr');
+    row.innerHTML = `
+      <td>${curso.titulo}</td>
+    `;
+    contenedorCarrito.appendChild(row);
+    console.log('Añadiendo curso', curso);
+  });
+}
+
+// borra el HTML del carrito
+function limpiarHTML() {
+  while (contenedorCarrito.firstChild) {
+    contenedorCarrito.removeChild(contenedorCarrito.firstChild);
   }
 }
