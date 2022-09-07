5b856b61adfbf806cc04dac073fae8e85b142cda
jose.ocana <jose.ocana@i3television.es>
2022-09-07 12:00:55 +0200

    [17] - Generar un select de años

--- 17-PROYECTO-Buscador/js/app.js
+++ 17-PROYECTO-Buscador/js/app.js
@@ -1,17 +1,35 @@
 // Variables
 const resultado = document.querySelector('#resultado');
+const year = document.querySelector('#year');
 
 // Eventos
 document.addEventListener('DOMContentLoaded', () => {
-  autos.forEach(auto => {
-    console.log('auto: ', auto);
+  mostrarResultado();
+  llenarYears();
+});
 
+// Funciones
+// Mostrar resultados
+function mostrarResultado() {
+  autos.forEach(auto => {
     const { marca, modelo, color, precio, puertas, transmision, year } = auto;
 
     const autoHTML = document.createElement('p');
     autoHTML.textContent = `${marca} - ${modelo} - ${color} - ${precio} - ${puertas} - ${transmision} - ${year}`;
     resultado.appendChild(autoHTML);
   });
-});
+}
 
-// Funciones
+// Rellenar combo de años
+function llenarYears() {
+  const fecha = new Date();
+  const max = fecha.getFullYear();
+  const min = max - 10;
+
+  for (let i = max; i >= min; i--) {
+    const opcion = document.createElement('option');
+    opcion.value = i;
+    opcion.textContent = i;
+    year.appendChild(opcion);
+  }
+}
